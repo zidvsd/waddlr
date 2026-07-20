149c6b384d2465d6d09bcde4280dc6f5e9b3f705
Zid <rashidvisda@gmail.com>
2026-07-20 23:15:36 +0800

    Revert "fix: cache auth client"
    This reverts commit f11ac453a91e4acf581b83b50e0109f9fa6807e0.

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -5,19 +5,8 @@ import * as schema from "./schema"
 
 const connectionString = process.env.DATABASE_URL!
 
-const globalForDb = globalThis as unknown as {
-  client: postgres.Sql | undefined
-}
+const client = postgres(connectionString, { prepare: false })
 
-const client =
-  globalForDb.client ??
-  postgres(connectionString, {
-    prepare: false,
-    max: 10, // cap pool size explicitly
-  })
-
-if (process.env.NODE_ENV !== "production") {
-  globalForDb.client = client
-}
-
-export const db = drizzle(client, { schema })
+export const db = drizzle(client, {
+  schema,
+})
